c01323f3e5ebfe0883a7dae2e1e24f161996fa39
Matt Hamada <ermacaz@gmail.com>
2023-03-03 20:38:36 -0700

    checkpoint

---
+++ bg-loader.js
@@ -0,0 +1,17 @@
+try {
+  importScripts("src/js/allBg.js");
+} catch (e) {
+  console.error(e);
+}
+
+try {
+  importScripts("src/js/transloader.js");
+} catch (e) {
+  console.error(e);
+}
+
+try {
+  importScripts( "background.js");
+} catch (e) {
+  console.error(e);
+}
